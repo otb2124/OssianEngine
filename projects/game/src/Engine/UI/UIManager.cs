c55ca57e37f6ebe22111af2220854aafe5c9a80e
arthvrm <153835086+arthvrm@users.noreply.github.com>
2025-02-26 10:42:16 +0200

    ui

--- projects/game/src/Engine/UI/UIManager.cs
+++ projects/game/src/Engine/UI/UIManager.cs
@@ -11,10 +11,12 @@ namespace UI
 {
     public class UIManager
     {
+        public List<UIComponent> components;
 
-        public UIManager() 
+
+        public UIManager()
         {
-
+            components = new List<UIComponent>();
         }
 
         public void Init()
@@ -25,6 +27,12 @@ namespace UI
         public void Draw()
         {
             //Graphics.Graphics.sprites.Draw(Resources.ResourceLoader.sprites[0].texture, Vector2.One, new Rectangle(0, 0, 64, 64), Color.White, 0f, Vector2.Zero, Vector2.One, SpriteEffects.FlipVertically, 0f);
+            {
+                for (int i = 0; i < components.Count; i++)
+                {
+                    components[i].Draw();
+                }
+            }
         }
     }
 }
