2855ef61580ebad0e170bc9f75e5916b81db9ef7
orest.brukhal <otb2124@g.rit.edu>
2025-07-02 19:38:10 +0200

    grounding platform update

--- projects/game/src/Engine/Entities/Entities/PhysicalEntities/PlatformEntity.cs
+++ projects/game/src/Engine/Entities/Entities/PhysicalEntities/PlatformEntity.cs
@@ -6,6 +6,7 @@ using Resources;
 using System.Collections.Generic;
 using static Resources.StaticSpriteFactory;
 using Utils;
+using System;
 
 
 namespace Entities
@@ -13,14 +14,53 @@ namespace Entities
     public class PlatformEntity : Entity
     {
 
+        public static Dictionary<Vector2, int[][]> layoutToIndicies = new()
+        {
+            {
+                //vert flip
+                new Vector2(2, 1),
+                new int[][]
+                {
+                    new int[]{ 2, 3 },
+                }
+            },
+            { 
+                //vert flip
+                new Vector2(3, 1),
+                new int[][]
+                {
+                    new int[]{ 2, 5, 3 },
+                }
+            },
+            {
+                new Vector2(3, 2),
+                new int[][]
+                {
+                    new int[]{ 0, 4, 1 },
+                    new int[]{ 2, 5, 3 },
+                }
+            },
+            {
+                new Vector2(3, 3),
+                new int[][]
+                {
+                    new int[]{ 0, 4, 1 },
+                    new int[]{ 11, 13, 12 },
+                    new int[]{ 2, 5, 3 },
+                }
+            },
+        };
+
         public FlatBody Body;
         public AnimationManager[] aManagers;
         public int[][] Indicies;
+        public bool IsGrounding;
 
-        public PlatformEntity(Vector2 pos, Point layout, float rot = 0f) : base()
+        public PlatformEntity(Vector2 pos, Point layout, float rot = 0f, bool isGrounding = false) : base()
         {
-            this.Indicies = GenerateIndicies(layout.X, layout.Y);
-            this.Body = FlatBodyFactory.createFlatBody(BodyDynamics.STATIC, BodyShapeType.Box, new Vector2(32*layout.X, 32*layout.Y), 1f, 0.5f);
+            this.Indicies = GenerateIndicies(layout.X, layout.Y, isGrounding);
+            this.Body = FlatBodyFactory.createFlatBody(BodyDynamics.STATIC, BodyShapeType.Box, new Vector2(32 * layout.X, 32 * layout.Y), 1f, 0.5f);
+            IsGrounding = isGrounding;
             Init(pos, rot);
         }
 
@@ -54,9 +94,9 @@ namespace Entities
             Graphics.Graphics.shapes.DrawBoxFill(FlatConverter.ToVector2(Body.Position), Body.Width, Body.Height, Body.Angle, drawColor);
         }
 
-        private static int[][] GenerateIndicies(int width, int height)
+        private static int[][] GenerateIndicies(int width, int height, bool isGrounding)
         {
-            if (layoutToIndicies.TryGetValue(new Vector2(width, height), out int[][] predefined))
+            if (!isGrounding && layoutToIndicies.TryGetValue(new Vector2(width, height), out int[][] predefined))
             {
                 return predefined;
             }
@@ -78,14 +118,16 @@ namespace Entities
                     }
                     else
                     {
-                        if (y == 0)
+                        if (y == 0 && !isGrounding)
                         {
-                            if (x == 0)
-                                indicies[y][x] = 0; // Outer corner left top
-                            else if (x == width - 1)
-                                indicies[y][x] = 1; // Outer corner right top
-                            else
-                                indicies[y][x] = 4; // Top border
+                                if (x == 0)
+                                    indicies[y][x] = 0; // Outer corner left top
+                                else if (x == width - 1)
+                                    indicies[y][x] = 1; // Outer corner right top
+                                else
+                                    indicies[y][x] = 4; // Top border
+
+                            
                         }
                         else if (y == height - 1)
                         {
@@ -111,98 +153,76 @@ namespace Entities
             return indicies;
         }
 
-
-        public static Dictionary<Vector2, int[][]> layoutToIndicies = new()
-        {
-            {
-                //vert flip
-                new Vector2(2, 1),
-                new int[][]
-                {
-                    new int[]{ 2, 3 },
-                }
-            },
-            { 
-                //vert flip
-                new Vector2(3, 1), 
-                new int[][]
-                {
-                    new int[]{ 2, 5, 3 },
-                } 
-            },
-            { 
-                //vert flip
-                new Vector2(4, 1),
-                new int[][]
-                {
-                    new int[]{ 2, 5, 5, 3 },
-                }
-            },
-            { 
-                //vert flip
-                new Vector2(5, 1),
-                new int[][]
-                {
-                    new int[]{ 2, 5, 5, 5, 3 },
-                }
-            },
-            {
-                new Vector2(3, 2),
-                new int[][]
-                {
-                    new int[]{ 0, 4, 1 },
-                    new int[]{ 2, 5, 3 },
-                }
-            },
-            {
-                new Vector2(3, 3),
-                new int[][]
-                {
-                    new int[]{ 0, 4, 1 },
-                    new int[]{ 11, 13, 12 },
-                    new int[]{ 2, 5, 3 },
-                }
-            },
-        };
-
         public override void Draw()
         {
-            //0 outer corner left top
-            //1 outer corner right top
-            //2 outer corner left bottom
-            //3 outer corner right bottom
-            //4 top border
-            //5 bottom border
-            //6 inner corner right top
-            //7 inner corner left top
-            //8 inner corner right bottom
-            //9 inner corner left bottom
-            //10 pillar top
-            //11 left border
-            //12 right border
-            //13 inner
-            //14 inner alt
-            //15 pillar bottom
+            // Tile indices:
+            // 0: outer corner left top
+            // 1: outer corner right top
+            // 2: outer corner left bottom
+            // 3: outer corner right bottom
+            // 4: top border
+            // 5: bottom border
+            // 6: inner corner right top
+            // 7: inner corner left top
+            // 8: inner corner right bottom
+            // 9: inner corner left bottom
+            // 10: pillar top
+            // 11: left border
+            // 12: right border
+            // 13: inner
+            // 14: inner alt
+            // 15: pillar bottom
 
             Matrix rotationMatrix = Matrix.CreateRotationZ(Body.Angle);
 
+            // Draw normal tiles
             for (int x = 0; x < Indicies.Length; x++)
             {
                 for (int y = 0; y < Indicies[0].Length; y++)
                 {
-
                     Vector2 localPos = new Vector2(y * 32, x * 32);
                     Vector2 rotatedPos = Vector2.Transform(localPos, rotationMatrix);
                     Vector2 worldPos = FlatConverter.ToVector2(Body.Position) + rotatedPos;
 
+                    aManagers[Indicies[x][y]].GetCurrent().Draw(
+                        worldPos,
+                        Color.White,
+                        this.Body.Angle,
+                        new Vector2(Body.Width / 2f, Body.Height / 2f),
+                        Vector2.One,
+                        0f,
+                        true
+                    );
+                }
+            }
 
-                    aManagers[Indicies[x][y]].GetCurrent().Draw(worldPos, Color.White, this.Body.Angle, new Vector2(Body.Width / 2f, Body.Height/2f), Vector2.One, 0f, true);
+            // Draw grounding rows if IsGrounding is true
+            if (IsGrounding)
+            {
+                for (int x = Indicies.Length; x < Indicies.Length + 25; x++)
+                {
+                    for (int y = 0; y < Indicies[0].Length; y++)
+                    {
+                        int tileIndex = (x + y) % 2 == 0 ? 13 : 14;
+                        Vector2 localPos = new Vector2(y * 32, x * 32);
+                        Vector2 rotatedPos = Vector2.Transform(localPos, rotationMatrix);
+                        Vector2 worldPos = new Vector2(FlatConverter.ToVector2(Body.Position).X + rotatedPos.X, FlatConverter.ToVector2(Body.Position).Y - rotatedPos.Y + (Indicies.Length-1) * 32);
+
+                        aManagers[tileIndex].GetCurrent().Draw(
+                            worldPos,
+                            Color.White,
+                            this.Body.Angle,
+                            new Vector2(Body.Width / 2f, Body.Height / 2f),
+                            Vector2.One,
+                            0f,
+                            true
+                        );
+                    }
                 }
             }
         }
-
-
     }
+
 
 
 }
--- projects/game/src/Engine/Entities/EntityMaps/EntitySetter.cs
+++ projects/game/src/Engine/Entities/EntityMaps/EntitySetter.cs
@@ -25,33 +25,35 @@ namespace Entities
 
                     //entities.Add(new PlatformEntity(new Vector2(0, -50), 47, 3, 2, 0.2f));
                     //entities.Add(new PlatformEntity(new Vector2(150, -50), 48, 5, 2));
-                    entities.Add(new PlatformEntity(new Vector2(-75, -200), new Point(3, 3)));
-                    entities.Add(new PlatformEntity(new Vector2(50, -200), new Point(3, 1)));
-                    entities.Add(new PlatformEntity(new Vector2(200, -200), new Point(2, 1)));
-                    entities.Add(new PlatformEntity(new Vector2(400, -200), new Point(3, 2)));
-                    entities.Add(new PlatformEntity(new Vector2(600, -200), new Point(3, 2), 20));
-                    entities.Add(new PlatformEntity(new Vector2(800, -200), new Point(3, 2)));
-                    entities.Add(new PlatformEntity(new Vector2(1000, -200), new Point(3, 2)));
-                    entities.Add(new PlatformEntity(new Vector2(1200, -200), new Point(3, 2)));
-                    entities.Add(new PlatformEntity(new Vector2(350, -350), new Point(20, 1)));
-                    entities.Add(new PlatformEntity(new Vector2(-350, 200), new Point(15, 1)));
+                    entities.Add(new PlatformEntity(new Vector2(-75,  0), new Point(3, 3)));
+                    entities.Add(new PlatformEntity(new Vector2(50,   0), new Point(3, 1)));
+                    entities.Add(new PlatformEntity(new Vector2(200,  0), new Point(2, 1)));
+                    entities.Add(new PlatformEntity(new Vector2(400,  0), new Point(3, 2)));
+                    entities.Add(new PlatformEntity(new Vector2(600,  0), new Point(3, 2), 20));
+                    entities.Add(new PlatformEntity(new Vector2(800,  0), new Point(3, 2)));
+                    entities.Add(new PlatformEntity(new Vector2(1000, 0), new Point(2, 2)));
+                    entities.Add(new PlatformEntity(new Vector2(1200, 0), new Point(3, 2)));
+                    entities.Add(new PlatformEntity(new Vector2(-150, 200), new Point(15, 1)));
+                    entities.Add(new PlatformEntity(new Vector2(0, -250), new Point(20, 1), 0, true));
+                    entities.Add(new PlatformEntity(new Vector2(-500, -230), new Point(12, 2), 0, true));
+                    entities.Add(new PlatformEntity(new Vector2(500, -230), new Point(12, 2), 0, true));
 
-                    entities.Add(new PhysicalEntity(Models.CRATE_0, new Vector2(-100, -50)));
-                    entities.Add(new PhysicalEntity(Models.CRATE_0, new Vector2(-200, -50)));
-                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 0)));
-                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 30)));
-                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 50)));
-                    entities.Add(new PhysicalEntity(Models.CRATE_1, new Vector2(-50, -150)));
-                    entities.Add(new PhysicalEntity(Models.CRATE_1, new Vector2(-65, -150)));
-                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(-70, -150)));
+                    entities.Add(new PhysicalEntity(Models.CRATE_0, new Vector2(-100, 150)));
+                    entities.Add(new PhysicalEntity(Models.CRATE_0, new Vector2(-200, 150)));
+                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 200)));
+                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 230)));
+                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(0, 250)));
+                    entities.Add(new PhysicalEntity(Models.CRATE_1, new Vector2(-50, 50)));
+                    entities.Add(new PhysicalEntity(Models.CRATE_1, new Vector2(-65, 50)));
+                    entities.Add(new PhysicalEntity(Models.BALL, new Vector2(-70, 50)));
 
-                    entities.Add(new Mob(new Vector2(-60, -140), 0f));
-                    entities.Add(new Mob(new Vector2(-150, -140), 0f));
+                    entities.Add(new Mob(new Vector2(-60, 100), 0f));
+                    entities.Add(new Mob(new Vector2(-150, 100), 0f));
 
-                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(-20, 100)));
-                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(20, 115)));
-                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(30, 110)));
-                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(40, 105)));
+                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(-20, 300)));
+                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(20, 335)));
+                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(30, 310)));
+                    entities.Add(new InteractiveItemEntity(InteractiveItemEntity.InteractiveItems.GOLD_COIN, new Vector2(40, 305)));
 
                     entities.Add(new InteractiveItemEntity(StaticSpriteFactory.GetItemUISpriteByItemKey(new ItemKey(ItemLib.Weapons.TERRABLADE)), FlatBodyPreset.ITEM_DROP, new Vector2(0, 300), new Vector2(30, 30), InteractiveItemEntity.InteractiveItemType.PICKUP, new Inventory(new ItemKey[] {new ItemKey(ItemLib.Weapons.TERRABLADE)})));
 
