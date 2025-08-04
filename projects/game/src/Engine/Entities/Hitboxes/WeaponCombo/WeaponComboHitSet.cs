176765badfb9a54c00d98d4353672913e0f56304
orest.brukhal <otb2124@g.rit.edu>
2025-08-04 16:39:34 +0200

    Revert "combo variety initial commit"
    This reverts commit e4efe04215861e4efd35f885c91bc95ad16776dd.

--- projects/game/src/Engine/Entities/Hitboxes/WeaponCombo/WeaponComboHitSet.cs
+++ projects/game/src/Engine/Entities/Hitboxes/WeaponCombo/WeaponComboHitSet.cs
@@ -1,72 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using static Entities.WeaponEntity;
 
 namespace Entities
 {
     public class WeaponComboHitSet
     {
+
         public WeaponComboHit[] Combohits;
-        public AttackType[] AttackSequence;
         public int CurrentComboHitId;
+
         public float ContinuationAllowCounter;
         public float ContinuationAllowTimeSec;
-        public bool AllowContinuation;
+        public bool AllowContinuation; 
 
-        public WeaponComboHitSet(WeaponComboHit[] hits, AttackType[] attackSequence)
+        public WeaponComboHitSet(WeaponComboHit[] hits)
         {
-            if (hits.Length != attackSequence.Length)
-                throw new ArgumentException("Hits and attack sequence lengths must match.");
-
-            Combohits = hits;
-            AttackSequence = attackSequence;
             CurrentComboHitId = 0;
             ContinuationAllowTimeSec = 0.5f;
-            ContinuationAllowCounter = 0;
+            Combohits = hits;
             AllowContinuation = false;
+            ContinuationAllowCounter = 0;
         }
 
         public WeaponComboHit GetCurrentHit()
         {
-            if (CurrentComboHitId < 0 || CurrentComboHitId >= Combohits.Length)
-            {
-                CurrentComboHitId = 0; // Reset to prevent crash
-            }
             return Combohits[CurrentComboHitId];
-        }
-
-        public bool CanContinueWith(AttackType attackType)
-        {
-            bool canContinue = AllowContinuation &&
-                              ContinuationAllowCounter < ContinuationAllowTimeSec &&
-                              CurrentComboHitId + 1 < AttackSequence.Length &&
-                              AttackSequence[CurrentComboHitId + 1] == attackType;
-            Console.WriteLine($"CanContinueWith: Attack {attackType}, Next {AttackSequence.ElementAtOrDefault(CurrentComboHitId + 1)}, Allow {AllowContinuation}, Timer {ContinuationAllowCounter}/{ContinuationAllowTimeSec}, CanContinue {canContinue}");
-            return canContinue;
-        }
-
-        public bool MatchesSequence(List<AttackType> history)
-        {
-            int minLength = Math.Min(AttackSequence.Length, history.Count);
-            for (int i = 0; i < minLength; i++)
-            {
-                if (AttackSequence[i] != history[history.Count - minLength + i])
-                    return false;
-            }
-            return true;
-        }
-
-        public int GetMatchLength(List<AttackType> history)
-        {
-            int minLength = Math.Min(AttackSequence.Length, history.Count);
-            for (int i = 0; i < minLength; i++)
-            {
-                if (AttackSequence[i] != history[history.Count - minLength + i])
-                    return i;
-            }
-            return minLength;
         }
 
         public void UpdateCounter(float deltaTime)
@@ -83,11 +45,9 @@ namespace Entities
 
         public void UpdateSet()
         {
-            CurrentComboHitId++;
-            if (CurrentComboHitId >= Combohits.Length)
-                CurrentComboHitId = Combohits.Length - 1; // Stay at last hit
+            CurrentComboHitId = (CurrentComboHitId + 1) % Combohits.Length;
             ContinuationAllowCounter = 0f;
-            AllowContinuation = CurrentComboHitId < Combohits.Length - 1;
+            AllowContinuation = true;
         }
 
         public void ResetCombo()
@@ -100,7 +60,7 @@ namespace Entities
         public void StartContinuationWindow()
         {
             ContinuationAllowCounter = 0f;
-            AllowContinuation = CurrentComboHitId < Combohits.Length - 1;
+            AllowContinuation = true;
         }
     }
 }
--- projects/game/src/Engine/Entities/Hitboxes/WeaponCombo/WeaponComboHitSetFactory.cs
+++ projects/game/src/Engine/Entities/Hitboxes/WeaponCombo/WeaponComboHitSetFactory.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Entities.WeaponEntity;
 
 namespace Entities
 {
@@ -16,71 +15,42 @@ namespace Entities
         }
 
 
-        private static readonly Dictionary<WeaponComboHitSets, (WeaponComboHit[], AttackType[])[]> comboHitTemplates = new()
+        private static readonly Dictionary<WeaponComboHitSets, WeaponComboHit[][]> comboHitTemplates = new()
         {
             {
                 WeaponComboHitSets.SWORD,
-                new (WeaponComboHit[], AttackType[])[]
+                new WeaponComboHit[][]
                 {
-                    // Combo 0: X X X (Light, Light, Light)
-                    (
-                        new WeaponComboHit[]
-                        {
-                            new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 0.5f),
-                            new WeaponComboHit(1, new Vector2(0, 10), 1f, new Vector2(20, 0), 0.5f),
-                            new WeaponComboHit(2, new Vector2(0, 10), 2f, new Vector2(30, 0), 0.7f),
-                        },
-                        new AttackType[] { AttackType.Light, AttackType.Light, AttackType.Light }
-                    ),
-                    // Combo 1: Y Y (Heavy, Heavy)
-                    (
-                        new WeaponComboHit[]
-                        {
-                            new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 1f),
-                            new WeaponComboHit(1, new Vector2(0, 10), 2f, new Vector2(30, 0), 1f),
-                        },
-                        new AttackType[] { AttackType.Heavy, AttackType.Heavy }
-                    ),
-                    // Combo 2: X X Y (Light, Light, Heavy)
-                    (
-                        new WeaponComboHit[]
-                        {
-                            new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 0.5f),
-                            new WeaponComboHit(1, new Vector2(0, 10), 1f, new Vector2(20, 0), 0.5f),
-                            new WeaponComboHit(2, new Vector2(15, 10), 2f, new Vector2(30, 0), 1f),
-                        },
-                        new AttackType[] { AttackType.Light, AttackType.Light, AttackType.Heavy }
-                    ),
-                    // Combo 3: X Y X Y (Light, Heavy, Light, Heavy)
-                    (
-                        new WeaponComboHit[]
-                        {
-                            new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 0.5f),
-                            new WeaponComboHit(1, new Vector2(15, 10), 2f, new Vector2(30, 0), 1f),
-                            new WeaponComboHit(2, new Vector2(0, 10), 1f, new Vector2(20, 0), 0.5f),
-                            new WeaponComboHit(3, new Vector2(15, 10), 2f, new Vector2(30, 0), 1f),
-                        },
-                        new AttackType[] { AttackType.Light, AttackType.Heavy, AttackType.Light, AttackType.Heavy }
-                    )
+                    new WeaponComboHit[] // Combo 0 (Light Attack)
+                    {
+                        new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 0.5f),
+                        new WeaponComboHit(1, new Vector2(0, 10), 1f, new Vector2(20, 0), 0.5f),
+                        new WeaponComboHit(2, new Vector2(0, 10), 2f, new Vector2(30, 0), 0.7f),
+                    },
+                    new WeaponComboHit[] // Combo 1 (Heavy Attack)
+                    {
+                        new WeaponComboHit(0, new Vector2(0, 10), 1.7f, Vector2.Zero, 1f),
+                        new WeaponComboHit(1, new Vector2(0, 10), 2f, new Vector2(30, 0), 1f),
+                    }
                 }
             }
         };
 
-        public static WeaponComboHitSet[] GetWeaponComboHitSets(WeaponComboHitSets weaponType)
-        {
-            var templates = comboHitTemplates[weaponType];
-            var sets = new WeaponComboHitSet[templates.Length];
-            for (int i = 0; i < templates.Length; i++)
+            public static WeaponComboHitSet[] GetWeaponComboHitSets(WeaponComboHitSets weaponType)
             {
-                sets[i] = new WeaponComboHitSet(templates[i].Item1, templates[i].Item2);
+                var templates = comboHitTemplates[weaponType];
+                var sets = new WeaponComboHitSet[templates.Length];
+                for (int i = 0; i < templates.Length; i++)
+                {
+                    sets[i] = new WeaponComboHitSet(templates[i]);
+                }
+                return sets;
             }
-            return sets;
-        }
 
         public static int GetTotalComboHits(WeaponComboHitSets weaponType)
         {
             var templates = comboHitTemplates[weaponType];
-            return templates.Sum(template => template.Item1.Length);
+            return templates.Sum(template => template.Length);
         }
     }
 
