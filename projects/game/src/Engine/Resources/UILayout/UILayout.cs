2a2bed9540ce7be5c74305cf6ff6c64211fe3c23
orest.brukhal <otb2124@g.rit.edu>
2026-03-20 14:53:46 +0100

    uilayout resources initial commit

---
+++ projects/game/src/Engine/Resources/UILayout/UILayout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources
+{
+    public class UILayout: Resource
+    {
+
+        public UILayout(string path): base(path) 
+        {
+
+        }
+
+    }
+}
