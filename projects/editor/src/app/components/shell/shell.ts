c5a167719f2f04ca5934ffa2edcb4be97babb3b2
orest.brukhal <otb2124@g.rit.edu>
2026-04-22 15:47:18 +0200

    repository removed editor and renamed engine

--- projects/editor/src/app/components/shell/shell.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-shell',
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
-})
-export class Shell {}
--- projects/editor/src/app/model/project-config.model.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectRecord } from "./project-record.model";
-
-export interface ProjectConfig {
-    projectRecord?: ProjectRecord;
-    resDirectory?: string;
-    targetDirectory?: string;
-}
